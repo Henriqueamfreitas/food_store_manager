a1108e8dab310220581ff77e440d2fc951c20ca8
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-07-01 09:31:16 -0300

    feat: tarefa 3 finalizada

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ from menu import products
 from management.product_handler import (
     get_product_by_id,
     get_products_by_type,
-    add_product
+    add_product,
+    menu_report
 )
 from management.tab_handler import calculate_tab
 
 if __name__ == "__main__":
     # TAREFA 1
-    # print(get_product_by_id(3000000000000000000))
-
-    # print(get_products_by_type("frut"))
-
+    print(get_product_by_id(3))
+    print(get_products_by_type("fruit"))
 
     # TAREFA 2
-    # new_product = {
-    #     "title": "X-Python",
-    #     "price": 5.0,
-    #     "rating": 5,
-    #     "description": "Sanduiche de Python",
-    #     "type": "fast-food"
-    # }
-    # print(add_product(products, **new_product))
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+    print(add_product(products, **new_product))
 
     table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
     table_2 = [
@@ -35,3 +34,15 @@ if __name__ == "__main__":
     # output
     # {'subtotal': '$216.1'}
     # {'subtotal': '$188.29'}
+
+    # TAREFA 3
+    try:
+        print(get_product_by_id("3000000000000000000"))
+    except TypeError as e:
+        print(str(e))
+
+    try:
+        print(get_products_by_type(3))
+    except TypeError as e:
+        print(str(e))
+    print(menu_report())
--- management/product_handler.py
+++ management/product_handler.py
@@ -2,17 +2,21 @@ from menu import products
 
 
 def get_product_by_id(product_id):
-    for element in products:
-        if (element["_id"] == product_id):
-            return element
+    if not isinstance(product_id, int):
+        raise TypeError("product id must be an int")
+    for product in products:
+        if (product["_id"] == product_id):
+            return product
     return {}
 
 
 def get_products_by_type(product_type):
+    if not isinstance(product_type, str):
+        raise TypeError("product type must be a str")
     product_type_list = []
-    for element in products:
-        if (element["type"] == product_type):
-            product_type_list.append(element)
+    for product in products:
+        if (product["type"] == product_type):
+            product_type_list.append(product)
     return product_type_list
 
 
@@ -29,3 +33,28 @@ def add_product(menu, **kwargs):
     menu.append(kwargs)
 
     return kwargs
+
+
+def menu_report():
+    product_count = len(products)
+
+    unique_types = []
+    all_product_types = []
+    total_price = 0
+    for product in products:
+        total_price += product["price"]
+        all_product_types.append(product["type"])
+        if product["type"] not in unique_types:
+            unique_types.append(product["type"])
+
+    average_price = round(total_price / product_count, 2)
+
+    most_common_type_count = 0
+    most_common_type = ""
+    for product_type in unique_types:
+        type_count = all_product_types.count(product_type)
+        if type_count > most_common_type_count:
+            most_common_type = product_type
+            most_common_type_count = type_count
+
+    return f"Products Count: {product_count} - Average Price: ${average_price} - Most Common Type: {most_common_type}"
